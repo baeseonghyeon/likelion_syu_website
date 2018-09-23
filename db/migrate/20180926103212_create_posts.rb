d4b875dc2333fb11a9a09b3983188ad03b69c925
Seonghyeon <tjdgusdkf@likelion.org>
2018-09-23 10:21:10 +0000

    222

--- db/migrate/20180926103212_create_posts.rb
+++ db/migrate/20180926103212_create_posts.rb
@@ -4,17 +4,11 @@ class CreatePosts < ActiveRecord::Migration[5.0]
       t.string :title
       t.text :content
       t.belongs_to :user, foreign_key: true
-      # t.belongs_to :user, :foreign_key => 'user_id'
-      # t.belongs_to :user, foreign_key: 'user_id'
-      # t.references :user_id,  foreign_key: true
       t.string :category
       t.string :lecture
       t.datetime :limit
 
       t.timestamps
     end
-    # add_reference :user, foreign_key: {to_table: :users}
-    # add_foreign_key :posts, :users, column: :user_id
-    # add_foreign_key :posts, references: :users
   end
 end
