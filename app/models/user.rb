f63899f235d89fe3157acc1689d18967bdcde8df
Seonghyeon <tjdgusdkf@likelion.org>
2018-10-04 15:17:22 +0000

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates_uniqueness_of :nickname
   mount_uploader :image, ImageUploader
 end
