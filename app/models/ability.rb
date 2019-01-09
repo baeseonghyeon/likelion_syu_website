651d096a8a93221b4815cd7ccdc06932ab46ec8e
Seonghyeon <tjdgusdkf@likelion.org>
2019-01-09 15:00:34 +0000

    user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,12 @@ class Ability
     #
       if user.nil? #비로그인 읽기만 가능
         can [:read,:notice,:homework,:lecture,:freeboard,:mypage,:mypost,:questions,:usershow], :all
-      elsif user.has_role? 'member' #멤버등급 읽기,쓰기, 본인글 수정 삭제
+      elsif user.has_role? 'member' #멤버등급 읽기,쓰기, 본인글 수정 삭제, 활동 멤버
         can [:read,:create,:notice,:homework,:lecture,:freeboard,:mypage,:mypost,:questions,:usershow], :all
         can [:update,:destroy], Post, user_id: user.id
+      elsif user.has_role? 'oldmember' #멤버등급 읽기,쓰기, 본인글 수정 삭제, 올드 멤버
+        can [:read,:create,:notice,:homework,:lecture,:freeboard,:mypage,:mypost,:questions,:usershow], :all
+        can [:update,:destroy], Post, user_id: user.id  
       elsif user.has_role? 'admin' #어드민, 모든글 접근, 어드민페이지 접근
         can [:read,:create,:update,:destory,:notice,:homework,:lecture,:mainnew,:servicenew,:usershow], :all
         can :access, :rails_admin
