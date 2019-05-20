1df4033861bfcc3b22bc108d2d9444275dd42ac1
Seonghyeon <tjdgusdkf@likelion.org>
2019-05-20 01:34:40 +0000

    front update

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -14,7 +14,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   end
   
   def size_range
-    1..10.megabytes
+    1..20.megabytes
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
