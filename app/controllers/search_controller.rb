cf519ca3877bd7ae3c7b612c0bffe4ef3900f055
Seonghyeon <tjdgusdkf@likelion.org>
2019-07-16 16:33:39 +0000

    aa

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
   
   def result
     @result = params[:search_text]
-    @posts_count = eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("title ILIKE ?", "%#{params[:search_text]}%")
-    @posts = eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("title ILIKE ?", "%#{params[:search_text]}%").where("content ILIKE ?", "%#{params[:search_text]}%").where("lecture ILIKE ?", "%#{params[:search_text]}%").order("created_at DESC").page(params[:page]).per(10)
+    @posts_count = eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("title ILIKE ?", "%#{params[:search_text]}%").or(eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("content ILIKE ?", "%#{params[:search_text]}%")).or(eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("lecture ILIKE ?", "%#{params[:search_text]}%"))
+    @posts = eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("title ILIKE ?", "%#{params[:search_text]}%").or(eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("content ILIKE ?", "%#{params[:search_text]}%")).or(eval(params[:param_posts]).where('created_at >= :years_ago', :years_ago => Time.now - 1.years).where("lecture ILIKE ?", "%#{params[:search_text]}%")).order("created_at DESC").page(params[:page]).per(10)
   end
 end
